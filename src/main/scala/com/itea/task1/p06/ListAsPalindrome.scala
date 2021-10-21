d0f22c5d1551c45987a32be4cf8f4de6edfa5b3d
Aleksandr Lazarchuk <aleksandr.lazarchuk@escalibre.net>
2021-10-21 06:27:25 +0300

    #master

--- src/main/scala/com/itea/task1/p06/ListAsPalindrome.scala
+++ src/main/scala/com/itea/task1/p06/ListAsPalindrome.scala
@@ -1,18 +1,17 @@
 package com.itea.task1.p06
 
 /**
- * Узнайте, является ли список палиндромом.
+ * Является ли список палиндромом.
  * @see http://aperiodic.net/phil/scala/s-99/p06.scala
  */
-object ListIsPalindrome extends App {
+
+class ListAsPalindrome {
   /*
    * проходит по списку дважды: один раз чтобы перевернуть его, и один раз чтобы проверить равенство
    */
   def isPalindrome[A](list: List[A]): Boolean = list == list.reverse
+}
 
-  println(
-    "isPalindrome (true) = " + isPalindrome(List(1, 2, 3, 2, 1)))  // isPalindrome (true) = true
-  println(
-    "isPalindrome (false) = " + isPalindrome(List(1, 2, 3, 4, 5))) // isPalindrome (false) = false
-
+object ListAsPalindrome {
+  def apply(): ListAsPalindrome = new ListAsPalindrome()
 }
--- src/test/scala/com/itea/task1/p06/ListIsPalindromeSpec.scala
+++ src/test/scala/com/itea/task1/p06/ListIsPalindromeSpec.scala
@@ -1,19 +1,16 @@
 package com.itea.task1.p06
 
-import collection.mutable.Stack
 import org.scalatest._
 import flatspec._
 import matchers._
 
 class ListIsPalindromeSpec extends AnyFlatSpec with should.Matchers {
 
-  def isPalindrome[A](list: List[A]): Boolean = list == list.reverse
-
   "Palindrome" should "should be true" in {
-    isPalindrome(List(1, 2, 3, 2, 1)) shouldBe true
+    ListAsPalindrome().isPalindrome(List(1, 2, 3, 2, 1)) shouldBe true
   }
 
   "Palindrome" should "should be false" in {
-    isPalindrome(List(1, 2, 3, 4, 5)) shouldBe false
+    ListAsPalindrome().isPalindrome(List(1, 2, 3, 4, 5)) shouldBe false
   }
 }
