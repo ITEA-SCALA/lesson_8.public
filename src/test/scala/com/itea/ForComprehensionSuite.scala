a681bb04498e36cc0d21d8cf6640c12d6380f7f5
Aleksandr Lazarchuk <aleksandr.lazarchuk@escalibre.net>
2021-10-25 01:40:30 +0300

    #master

--- src/test/scala/com/itea/ForComprehensionSuite.scala
+++ src/test/scala/com/itea/ForComprehensionSuite.scala
@@ -23,12 +23,13 @@ class ForComprehensionSuite extends AnyFunSuite with OptionValues{
 //                              Option
 //   None extends Option[Nothing]    Some(value)
 
-//    val option: Option[String] = opt1.flatMap(i => opt2.flatMap(j => opt3.map(k => i + j + k)))
+//    val option: Option[String] = opt1.flatMap(i => opt2.flatMap(j => opt3.map(k => i + j + k))) //TODO  операции выполняются до тех пор пока есть flatMap
     val option = for {
-      i <- opt1
-      j <- opt2
-      k <- opt3
-    } yield i + j + k
+      i <- opt1       // здесь выполняется 1-я операция
+      j <- opt2       // если 1-я операция прошла успешно, тогда выполняется 2-я операция
+      k <- opt3       // а если 2-я операция прошла успешно, тогда выполняется 3-я операция
+    } yield i + j + k // и если 3-я операция прошла успешно, тогда уже выполняется 4-я операция
+    // если где-то свалиться None, тогда метод flatMap у Option ничего не делает и просто вернет None
 
 //    assert(None == option)
     assert(option.isEmpty)
