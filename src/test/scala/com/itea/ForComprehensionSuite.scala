5b2bb728f142abd5c30d90bc7837d18b466caa59
Aleksandr Lazarchuk <aleksandr.lazarchuk@escalibre.net>
2021-10-25 02:57:23 +0300

    #master

--- src/test/scala/com/itea/ForComprehensionSuite.scala
+++ src/test/scala/com/itea/ForComprehensionSuite.scala
@@ -68,4 +68,16 @@ class ForComprehensionSuite extends AnyFunSuite with OptionValues with EitherVal
     assert(res.isEmpty)
   }
 
+  test("") {
+    :: (1, Nil) // это вызов конструктора для кейс-класса
+    ::.apply(1, Nil)
+
+    1 :: Nil    // в даном случае это будет уже метод на объекте
+
+    def compressRecursive[A] (ls: List[A]): List[A] = ls match {
+      case Nil => Nil
+      case h :: tail => h :: compressRecursive(tail.dropWhile(_ == h))
+    }
+  }
+
 }
