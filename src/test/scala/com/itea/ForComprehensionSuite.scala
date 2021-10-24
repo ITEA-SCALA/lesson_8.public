a0dfca50d0cc89c8ec2a57d60b4383d8d7c1b42c
Aleksandr Lazarchuk <aleksandr.lazarchuk@escalibre.net>
2021-10-25 01:31:59 +0300

    #master

--- src/test/scala/com/itea/ForComprehensionSuite.scala
+++ src/test/scala/com/itea/ForComprehensionSuite.scala
@@ -15,4 +15,23 @@ class ForComprehensionSuite extends AnyFunSuite with OptionValues{
     assert("1strtrue" == option.value)
   }
 
+  test("Option example - unhappy path") {
+    val opt1: Option[Int] = None
+    val opt2 = Some("str")
+    val opt3 = Some(true)
+
+//                              Option
+//   None extends Option[Nothing]    Some(value)
+
+//    val option: Option[String] = opt1.flatMap(i => opt2.flatMap(j => opt3.map(k => i + j + k)))
+    val option = for {
+      i <- opt1
+      j <- opt2
+      k <- opt3
+    } yield i + j + k
+
+//    assert(None == option)
+    assert(option.isEmpty)
+  }
+
 }
